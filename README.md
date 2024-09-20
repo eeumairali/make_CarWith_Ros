ccc968b7d911d438bfa97dd8b90a034ab3c8e9f8
eeumairali <eeumairali@gmail.com>
2024-09-20 22:09:56 +0500

    push to github to use it later to linux

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # make_CarWith_Ros
 - 1. [Follow_for_ROS_installation](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html#environment-setup)
 - 2. Now i stopped before <sudo apt install ros-humble-desktop>
+- 3. making turtle moving <sudo apt install ros-humble-turtlesim>
+- 4. echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+- 5. source ~/.bashrc
+- 6. ros2 run turtlesim turtlesim_node
+- 7. write script run turtle
+- 8. 
+
+
---
+++ run_turtle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class TurtleController(Node):
+    def __init__(self):
+        super().__init__('turtle_control')
+        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.move_turtle)
+        self.velocity = Twist()
+
+    def move_turtle(self):
+        self.velocity.linear.x = 2.0
+        self.velocity.angular.z = 1.0
+        self.publisher_.publish(self.velocity)
+
+def main(args=None):
+    rclpy.init(args=args)
+    turtle_controller = TurtleController()
+    rclpy.spin(turtle_controller)
+    turtle_controller.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
